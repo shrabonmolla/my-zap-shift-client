0736d26152bda765c8c4e566b7465f752d2805b0
shrabonmolla <shrabonmolla007@gmail.com>
2025-12-03 12:21:21 +0600

    implemented approved and reject rider

--- src/Pages/DashboardPages/Riders/ManageRiders.jsx
+++ src/Pages/DashboardPages/Riders/ManageRiders.jsx
@@ -6,13 +6,48 @@ import { BookmarkPlus, BookmarkX, ReceiptRussianRuble } from "lucide-react";
 export default function ManageRiders() {
   const axiosSecure = useAxiosSecure();
 
-  const { data } = useQuery({
+  const { data, refetch } = useQuery({
     queryKey: ["riders"],
     queryFn: async () => {
       const res = await axiosSecure.get("/riders");
       return res.data;
     },
   });
+
+  // handleApproveRider
+
+  // new metod
+  function updateRiderStatus(rider, status) {
+    const updateInfo = { status: status, email: rider.email };
+    axiosSecure
+      .patch(`/riders/${rider._id}`, updateInfo)
+      .then((res) => {
+        console.log("rider status update", res);
+        refetch();
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleApproveRider(rider) {
+    updateRiderStatus(rider, "approved");
+  }
+
+  function handleRejectRider(rider) {
+    updateRiderStatus(rider, "reject");
+  }
+
+  // old method
+
+  // function handleApproveRider(id) {
+  //   const updateInfo = { status: "approved" };
+  //   axiosSecure
+  //     .patch(`/riders/${id}`, updateInfo)
+  //     .then((res) => {
+  //       console.log("rider status update", res);
+  //       refetch();
+  //     })
+  //     .catch((err) => console.log(err));
+  // }
   console.log(data);
   return (
     <div className="overflow-x-auto">
@@ -29,25 +64,32 @@ export default function ManageRiders() {
           </tr>
         </thead>
         <tbody>
-          {data.map((rider, index) => {
-            return (
-              <tr key={index}>
-                <th>{index + 1}</th>
-                <td>{rider.name}</td>
-                <td>{rider.email}</td>
-                <td>{rider.district}</td>
-                <td>{rider.status}</td>
-                <td>
-                  <button className="btn">
-                    <BookmarkPlus />
-                  </button>
-                  <button className="btn">
-                    <BookmarkX />
-                  </button>
-                </td>
-              </tr>
-            );
-          })}
+          {data &&
+            data.map((rider, index) => {
+              return (
+                <tr key={index}>
+                  <th>{index + 1}</th>
+                  <td>{rider.name}</td>
+                  <td>{rider.email}</td>
+                  <td>{rider.district}</td>
+                  <td>{rider.status}</td>
+                  <td>
+                    <button
+                      onClick={() => handleApproveRider(rider)}
+                      className="btn"
+                    >
+                      <BookmarkPlus />
+                    </button>
+                    <button
+                      onClick={() => handleRejectRider(rider)}
+                      className="btn"
+                    >
+                      <BookmarkX />
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
     </div>
